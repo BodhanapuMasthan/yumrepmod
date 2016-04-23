14ab230d077bc18b3daa81b32258b48deb4d3178
root <root@PuppetMaster.node.com>
2016-04-23 17:35:34 +0530

    yum

---
+++ manifests/init.pp
@@ -0,0 +1,48 @@
+# == Class: yumrepmod
+#
+# Full description of class yumrepmod here.
+#
+# === Parameters
+#
+# Document parameters here.
+#
+# [*sample_parameter*]
+#   Explanation of what this parameter affects and what it defaults to.
+#   e.g. "Specify one or more upstream ntp servers as an array."
+#
+# === Variables
+#
+# Here you should define a list of variables that this module would require.
+#
+# [*sample_variable*]
+#   Explanation of how this variable affects the funtion of this class and if
+#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
+#   External Node Classifier as a comma separated list of hostnames." (Note,
+#   global variables should be avoided in favor of class parameters as
+#   of Puppet 2.6.)
+#
+# === Examples
+#
+#  class { 'yumrepmod':
+#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
+#  }
+#
+# === Authors
+#
+# Author Name <author@domain.com>
+#
+# === Copyright
+#
+# Copyright 2016 Your name here, unless otherwise noted.
+#
+class yumrepmod {
+
+  yumrepo { 'server.repo':
+    ensure   => 'present',
+    baseurl  => 'file:///mnt',
+    enabled  => '1',
+    gpgcheck => '0',
+}
+
+
+}
